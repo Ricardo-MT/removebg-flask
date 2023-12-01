a50f2b188c127756157e8816f0a4d8a83d7eef7a
Ricardo Mejias <ricardo.mejias@digibeat.com>
2023-12-01 12:31:37 +0100

    Add post request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import render_template
 
 app = Flask(__name__)
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.post("/remove_background/")
+def remove_background():
+    return jsonify({"message": "Hello, World!"})
